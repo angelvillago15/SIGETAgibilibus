37b9054ce4571db03ebb353cadcbca084be2670f
CarrivasUclm <carlos.rivas1@alu.uclm.es>
2020-10-18 10:42:00 +0200

    ReunionDAO

---
+++ src/main/java/com/agibilibus/SIGET/dao/ReunionDAO.java
@@ -0,0 +1,7 @@
+package com.agibilibus.SIGET.dao;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ReunionDAO extends MongoRepository <Reunion, String> {
+
+}
