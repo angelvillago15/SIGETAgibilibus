560abb24ed10f2ca2e530fe99fd97e0667b3c717
CarrivasUclm <carlos.rivas1@alu.uclm.es>
2020-10-18 11:03:37 +0200

    Funciona? Funciona

--- src/main/java/com/agibilibus/SIGET/SigetApplication.java
+++ src/main/java/com/agibilibus/SIGET/SigetApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.agibilibus.SIGET.dao.ReunionDAO;
 import com.agibilibus.SIGET.dao.UserDAO;
+import com.agibilibus.SIGET.model.Reunion;
 import com.agibilibus.SIGET.model.Usuario;
 
 
@@ -14,6 +16,9 @@ public class SigetApplication implements CommandLineRunner{
 	
 	@Autowired
 	UserDAO userdao;
+	
+	@Autowired
+	ReunionDAO reuniondao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SigetApplication.class, args);
@@ -21,6 +26,7 @@ public class SigetApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		//reuniondao.save(new Reunion(1,"Bienvenida", "Reunión de bienvenida", "23/10/2020", "12:00", "13:00",userdao.findById("carlos").get(),userdao.findAll(),"hola.com"));
 		//userdao.save(new Usuario("carlos", "aaa", "Carlos", "Rivas", 666, "a@a.com", "062x","10/05/1997", "admin"));
 	}
 
--- src/main/java/com/agibilibus/SIGET/dao/ReunionDAO.java
+++ src/main/java/com/agibilibus/SIGET/dao/ReunionDAO.java
@@ -2,6 +2,8 @@ package com.agibilibus.SIGET.dao;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.agibilibus.SIGET.model.Reunion;
+
 public interface ReunionDAO extends MongoRepository <Reunion, String> {
 
 }
