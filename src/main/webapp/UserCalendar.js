d95f6b938c26c158ac4fc46a46a6c04e01824bf6
Elisa <elisa.jimenez1@alu.uclm.es>
2020-10-23 11:09:34 +0200

    Vista nueva del calendario

--- src/main/webapp/UserCalendar.js
+++ src/main/webapp/UserCalendar.js
@@ -1,5 +1,95 @@
 
-  function openNav() {
+  function getSemana () {
+		var data = {
+
+				url : "getSemana",
+				type : "post",
+				
+				success : function(response) {
+
+					for (var i=0; i<response.length; i++) {
+					//se escribe el dia
+					$("#d"+i).val(response[i].fecha);
+						
+						var reunion = response[i].reuniones;
+						var html ="<table>";
+						for (var j=0; j<reunion.length; j++) {
+							html = html + "<tr><td>"+reunion[j].nombre+"</td></tr>";
+						}
+						html = html +"</table>"
+						$("#ls"+i).val(html);
+
+					}
+				},
+				error : function(response) {
+					alert(response.message);
+				}
+			};
+			$.ajax(data);
+	}
+	getSemana();
+	
+	function getSemanaSiguiente () {
+		alert("siguiente");
+		var data = {
+
+				url : "getSemanaSiguiente",
+				type : "post",
+				
+				success : function(response) {
+					for (var i=0; i<response.length; i++) {
+					jso = JSON.parse(response[i]);
+					//se escribe el dia
+					$("#d"+i).val(jso.fecha);
+						
+						var reunion = jso.reuniones;
+						var html ="<table>";
+						for (var j=0; j<reunion.length; j++) {
+							html = html + "<tr><td>"+reunion[j].nombre+"</td></tr>";
+						}
+						html = html +"</table>"
+						$("#ls"+i).val(html);
+
+					}
+				},
+				error : function(response) {
+					alert(response.message);
+				}
+			};
+			$.ajax(data);
+	}
+	 function getSemanaAnterior () {
+	 	alert("anterior");
+		var data = {
+				
+				url : "getSemanaAnterior",
+				type : "post",
+				
+				success : function(response) {
+					for (var i=0; i<response.length; i++) {
+					//se escribe el dia
+					$("#d"+i).val(response[i].fecha);
+						
+						var reunion = response[i].reuniones;
+						var html ="<table>";
+						for (var j=0; j<reunion.length; j++) {
+							html = html + "<tr><td>"+reunion[j].nombre+"</td></tr>";
+						}
+						html = html +"</table>"
+						$("#ls"+i).val(html);
+
+					}
+
+				},
+				error : function(response) {
+					alert(response.message);
+				}
+			};
+			$.ajax(data);
+	}
+
+
+function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
@@ -11,6 +101,7 @@
     document.body.style.backgroundColor = "white";
   }
 
+<<<<<<< HEAD
   $(document).ready(function() {
     $('#calendar').fullCalendar({
         header: { /*botones que hay arriba del calendario numerico*/
@@ -43,3 +134,6 @@
     });
     $('#calendar').fullCalendar( 'removeEvents').fullCalendar('removeEventSources');
   });
+=======
+
+>>>>>>> T1.4-ControladorCalendario
