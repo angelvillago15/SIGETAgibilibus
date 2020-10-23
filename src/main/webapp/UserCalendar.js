609444bd4dd6333ffac159dd8855b222b145ed56
Jaimej5 <43786797+Jaimej5@users.noreply.github.com>
2020-10-23 11:51:51 +0200

    Calendario en vista de usuario

--- src/main/webapp/UserCalendar.js
+++ src/main/webapp/UserCalendar.js
@@ -1,139 +1,104 @@
 
-  function getSemana () {
-		var data = {
+ function getSemana () {
+  var data = {
 
-				url : "getSemana",
-				type : "post",
-				
-				success : function(response) {
+      url : "getSemana",
+      type : "post",
+      
+      success : function(response) {
 
-					for (var i=0; i<response.length; i++) {
-					//se escribe el dia
-					$("#d"+i).val(response[i].fecha);
-						
-						var reunion = response[i].reuniones;
-						var html ="<table>";
-						for (var j=0; j<reunion.length; j++) {
-							html = html + "<tr><td>"+reunion[j].nombre+"</td></tr>";
-						}
-						html = html +"</table>"
-						$("#ls"+i).val(html);
+        for (var i=0; i<response.length; i++) {
+        //se escribe el dia
+        $("#d"+i).val(response[i].fecha);
+          
+          var reunion = response[i].reuniones;
+          var html ="<table>";
+          for (var j=0; j<reunion.length; j++) {
+            html = html + "<tr><td>"+reunion[j].nombre+"</td></tr>";
+          }
+          html = html +"</table>"
+          $("#ls"+i).val(html);
 
-					}
-				},
-				error : function(response) {
-					alert(response.message);
-				}
-			};
-			$.ajax(data);
-	}
-	getSemana();
-	
-	function getSemanaSiguiente () {
-		alert("siguiente");
-		var data = {
+        }
+      },
+      error : function(response) {
+        alert(response.message);
+      }
+    };
+    $.ajax(data);
+}
+getSemana();
 
-				url : "getSemanaSiguiente",
-				type : "post",
-				
-				success : function(response) {
-					for (var i=0; i<response.length; i++) {
-					jso = JSON.parse(response[i]);
-					//se escribe el dia
-					$("#d"+i).val(jso.fecha);
-						
-						var reunion = jso.reuniones;
-						var html ="<table>";
-						for (var j=0; j<reunion.length; j++) {
-							html = html + "<tr><td>"+reunion[j].nombre+"</td></tr>";
-						}
-						html = html +"</table>"
-						$("#ls"+i).val(html);
+function getSemanaSiguiente () {
+  alert("siguiente");
+  var data = {
 
-					}
-				},
-				error : function(response) {
-					alert(response.message);
-				}
-			};
-			$.ajax(data);
-	}
-	 function getSemanaAnterior () {
-	 	alert("anterior");
-		var data = {
-				
-				url : "getSemanaAnterior",
-				type : "post",
-				
-				success : function(response) {
-					for (var i=0; i<response.length; i++) {
-					//se escribe el dia
-					$("#d"+i).val(response[i].fecha);
-						
-						var reunion = response[i].reuniones;
-						var html ="<table>";
-						for (var j=0; j<reunion.length; j++) {
-							html = html + "<tr><td>"+reunion[j].nombre+"</td></tr>";
-						}
-						html = html +"</table>"
-						$("#ls"+i).val(html);
+      url : "getSemanaSiguiente",
+      type : "post",
+      
+      success : function(response) {
+        for (var i=0; i<response.length; i++) {
+        jso = JSON.parse(response[i]);
+        //se escribe el dia
+        $("#d"+i).val(jso.fecha);
+          
+          var reunion = jso.reuniones;
+          var html ="<table>";
+          for (var j=0; j<reunion.length; j++) {
+            html = html + "<tr><td>"+reunion[j].nombre+"</td></tr>";
+          }
+          html = html +"</table>"
+          $("#ls"+i).val(html);
 
-					}
+        }
+      },
+      error : function(response) {
+        alert(response.message);
+      }
+    };
+    $.ajax(data);
+}
+ function getSemanaAnterior () {
+   alert("anterior");
+  var data = {
+      
+      url : "getSemanaAnterior",
+      type : "post",
+      
+      success : function(response) {
+        for (var i=0; i<response.length; i++) {
+        //se escribe el dia
+        $("#d"+i).val(response[i].fecha);
+          
+          var reunion = response[i].reuniones;
+          var html ="<table>";
+          for (var j=0; j<reunion.length; j++) {
+            html = html + "<tr><td>"+reunion[j].nombre+"</td></tr>";
+          }
+          html = html +"</table>"
+          $("#ls"+i).val(html);
 
-				},
-				error : function(response) {
-					alert(response.message);
-				}
-			};
-			$.ajax(data);
-	}
+        }
+
+      },
+      error : function(response) {
+        alert(response.message);
+      }
+    };
+    $.ajax(data);
+}
 
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
-<<<<<<< HEAD
-  $(document).ready(function() {
-    $('#calendar').fullCalendar({
-        header: { /*botones que hay arriba del calendario numerico*/
-            left: 'prev,next',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-        },
-        
-        buttonIcons: true,
-        weekNumbers: false, /*Establecido en true muestra una pequeña columna en la parte izquierda del calendario para informar del número de la semana.*/ 
-        editable: true, /*Poner a false si se puede editar en plan dinámico*/
-        eventLimit: true, /*Permite limitar la cantidad de eventos que se muestran en un día. El resto se mostrará en una ventana emergente. Puedes indicar true, false o un número entero.*/
-        events: [ /*Definición de la lista de objetos o eventos que se mostrarán en el calendario. */
-            {
-                title: 'Reunion',
-                description: 'Reunion Vespertina',
-                start: '2019-07-01',
-                color: '#3A87AD',
-                textColor: '#ffffff',
-            }
-        ],
-        /*dayClick: function (date, jsEvent, view) {
-            alert('Has hecho click en: '+ date.format());
-        }, 
-        eventClick: function (calEvent, jsEvent, view) {
-            $('#event-title').text(calEvent.title);
-            $('#event-description').html(calEvent.description);
-            $('#modal-event').modal();
-        },*/  
-    });
-    $('#calendar').fullCalendar( 'removeEvents').fullCalendar('removeEventSources');
-  });
-=======
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
 
->>>>>>> T1.4-ControladorCalendario
+
