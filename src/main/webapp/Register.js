fac86dde8bbf7375113c2c66fb43c38d17a3651a
Jaimej5 <43786797+Jaimej5@users.noreply.github.com>
2020-10-25 12:53:13 +0100

    update final vista register.html e introduccion al controller en register.js

--- src/main/webapp/Register.js
+++ src/main/webapp/Register.js
@@ -41,6 +41,54 @@ $(document).ready(function() {
             alert("Rellena todos los campos");
         }else if(pass.length < 8 ||  !pass.match(/[A-z]/) || !pass.match(/\d/) || !pass.match(/[A-Z]/)){
             alert("La contraseña no tiene los requisitos requeridos");
+        }else{
+            alert("Usuario registrado");
+            window.location="Login.html";
+            limpiarCampos();
         }
     });
+
+    function limpiarCampos(){ // resetear todos los campos
+        document.getElementById("username").value="";
+        document.getElementById("dni").value="";
+        document.getElementById("name").value="";
+        document.getElementById("Apellidos").value="";
+        document.getElementById("telefono").value="";
+        document.getElementById("pass").value="";
+        document.getElementById("fechaN").value="";
+    };
+    class Reunion{
+        constructor(dni, nombre, apellidos, telefono, fechaN, usuario, password, mail){
+            this.dni=dni;
+            this.nombre=nombre;
+            this.apellidos=apellidos;
+            this.telefono=telefono;
+            this.fechaN=fechaN;
+            this.usuario=usuario;
+            this.password=password;
+            this.mail=mail;
+        }
+    
+        crearReunion(){
+            var msg ={
+                type : "nuevo usuario",
+                dni : this.dni;
+                nombre : this.nombre,
+                apellidos : this.apellidos,
+                telefono : this.telefono,
+                fechaN : this.fechaN,
+                usuario : this.usuario,
+                password : this.password,
+                mail : this.mail,
+                rol : "usuario"
+            };
+            var data = {
+                data : JSON.stringify(msg),
+                url : "nuevoUsuario",
+                type : "post",
+                contentType : 'application/json',
+                dataType : 'json',
+            }
+            //pasar a json JSON.stringify(msg)
+        }
 });
