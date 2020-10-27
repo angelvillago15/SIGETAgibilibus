2a7e24ea26f6537c700e4f61891f431a351bafb2
PILAR <Pilar.Hernan@alu.uclm.es>
2020-10-27 18:19:21 +0100

    Empiezo a cargar formulario

--- src/main/webapp/inviteEvent.js
+++ src/main/webapp/inviteEvent.js
@@ -1,4 +1,5 @@
-function reunion () { // se le llama cuando se da al boton crear
+function invitar () { // se le llama cuando se da al boton Invitar
+
 	
 	modificarReunion();//Aqui cargamos los datos de la reunion
 	var reunion= new Reunion($("#nombreReunion").val(),$("#fecha").val(),$("#horaInicio").val(),$("#horaFin").val(),$("#descripcion").val(),$("#url").val(),$("#invitar").val()); //obtenemos un objeto 
@@ -10,24 +11,13 @@ function reunion () { // se le llama cuando se da al boton crear
 };
 
 function cargarFormulario(){
-	document.getElementById("nombreReunion").value="";
-    document.getElementById("fecha").value="";
-    document.getElementById("horaInicio").value="";
-    document.getElementById("horaFin").value="";
-    document.getElementById("descripcion").value="";
-    document.getElementById("url").value="";
-    document.getElementById("invitar").value="";
+	
 };
 
 
 class Reunion{
     constructor(nombre, fecha, horaInicio, horaFin, descripcion, url,correos){
         this.nombre=nombre;
-        this.fecha=fecha;
-        this.horaInicio=horaInicio;
-        this.horaFin=horaFin;
-        this.descripcion=descripcion;
-        this.url=url;
         this.correos=correos;
     }
 
@@ -35,11 +25,6 @@ class Reunion{
         var msg ={
             type : "guardar Reunion",
 			nombre : this.nombre,
-            fecha : this.fecha,
-            horaInicio : this.horaInicio,
-			horaFin : this.horaFin,
-            descripcion : this.descripcion,
-            url: this.url,
             correos: this.correos
         };
         var data = {
