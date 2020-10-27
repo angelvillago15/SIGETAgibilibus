feba38812bbe3a392e580597a2d3655e5e27360c
PILAR <Pilar.Hernan@alu.uclm.es>
2020-10-27 19:31:24 +0100

    cojo datos para mostrar y envio reunion

--- src/main/webapp/inviteEvent.js
+++ src/main/webapp/inviteEvent.js
@@ -1,23 +1,41 @@
 function invitar () { // se le llama cuando se da al boton Invitar
-
 	
-	modificarReunion();//Aqui cargamos los datos de la reunion
-	var reunion= new Reunion($("#nombreReunion").val(),$("#fecha").val(),$("#horaInicio").val(),$("#horaFin").val(),$("#descripcion").val(),$("#url").val(),$("#invitar").val()); //obtenemos un objeto 
-	cargarFormulario();
-	guardarReunion();
-	alert('Se creó una reunion correctamente ');
+		var reunion= new Reunion($("#nombreReunion").val(),$("#fecha").val(),$("#horaInicio").val(),$("#horaFin").val(),$("#descripcion").val(),$("#url").val(),$("#invitar").val()); //obtenemos un objeto 
+		guardarReunion();
+		alert('Se creó una reunion correctamente ');
+	
 	window.location.href = "UserCalendar.html";
-  
 };
 
-function cargarFormulario(){
-	
+function loadEvento() {
+	var data = {
+
+		url : "loadEvento",
+		type : "post",
+		
+		success : function(response) {
+		
+			document.getElementById("nombreReunion").value=response.nombre;
+			document.getElementById("fecha").value=response.fecha;
+			document.getElementById("horaInicio").value=response.horaInicio;
+			document.getElementById("horaFin").value=response.horaFin;
+			document.getElementById("descripcion").value=response.descripcion;
+			document.getElementById("url").value=response.url;
+			document.getElementById("invitar").value=response.invitar;
+		}
+	};
 };
+
 
 
 class Reunion{
     constructor(nombre, fecha, horaInicio, horaFin, descripcion, url,correos){
-        this.nombre=nombre;
+    	this.nombre=nombre;
+        this.fecha=fecha;
+        this.horaInicio=horaInicio;
+        this.horaFin=horaFin;
+        this.descripcion=descripcion;
+        this.url=url;
         this.correos=correos;
     }
 
@@ -25,6 +43,11 @@ class Reunion{
         var msg ={
             type : "guardar Reunion",
 			nombre : this.nombre,
+			fecha : this.fecha,
+            horaInicio : this.horaInicio,
+			horaFin : this.horaFin,
+            descripcion : this.descripcion,
+            url: this.url,
             correos: this.correos
         };
         var data = {
