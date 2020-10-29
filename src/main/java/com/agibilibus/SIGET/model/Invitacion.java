e9686462f9e4623ebc33bf9a8cbdc42f39d990b9
Elisa <elisa.jimenez1@alu.uclm.es>
2020-10-29 12:12:42 +0100

    Invitacion.java

--- src/main/java/com/agibilibus/SIGET/model/Invitacion.java
+++ src/main/java/com/agibilibus/SIGET/model/Invitacion.java
@@ -1,5 +1,42 @@
 package com.agibilibus.SIGET.model;
 
 public class Invitacion {
+	private Usuario usuario;
+	private Reunion reunion;
+	private EstadoInvitacion estado;
+	
+	public Invitacion () {}
+	public Invitacion (Usuario usuario, Reunion reunion, EstadoInvitacion estado) {
+		this.usuario = usuario;
+		this.reunion = reunion;
+		this.estado = estado;
+		
+		
+	}
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Reunion getReunion() {
+		return reunion;
+	}
+	public void setReunion(Reunion reunion) {
+		this.reunion = reunion;
+	}
+	public EstadoInvitacion getEstado() {
+		return estado;
+	}
+	public void setEstado(EstadoInvitacion estado) {
+		this.estado = estado;
+	}
+	public void aceptarInvitacion() {}
+	public void rechazarInvitacion() {}
+	
+	
+
+
 
 }
