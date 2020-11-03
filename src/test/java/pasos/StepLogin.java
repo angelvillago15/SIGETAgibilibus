bc8b33636cac64bfa77dc63fc5a775c0d0ddb2e9
CarrivasUclm <carlos.rivas1@alu.uclm.es>
2020-11-03 02:03:30 +0100

    Cucumber con mvn test

--- src/test/java/pasos/StepLogin.java
+++ src/test/java/pasos/StepLogin.java
@@ -12,42 +12,32 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class StepLogin {
-	
-	
-	
+
 	@Given("estoy registrado como usuario ")
-	public void registro( ) {
+	public void registro() {
 		Usuario.get().register();
 	}
-	
+
 	@When("introduzco en el campo usuario <usuario> correctamente")
 	public void introducirUsuario(String usu, String pwd, HttpSession httpSession) throws Exception {
-		Sesion.get().login(httpSession,usu,pwd);
-		
+		Sesion.get().login(httpSession, usu, pwd);
+
 	}
+
 	@When("introduzco contraseña <password> correctamente")
 	public void introducirContraseña(String usu, String pwd, HttpSession httpSession) throws Exception {
-		Sesion.get().login(httpSession,usu,pwd);
+		Sesion.get().login(httpSession, usu, pwd);
 	}
-	
-	
-	@Then("voy a la pagina de inicio del sistema")
-	public void mostrarPantallaInicio(String url) {
-		//Manager.visualizaPaginaPrincipal();
-		
-	}
-	
+
 	@Then("Usuario es incorrecto, mostrar el mensaje <mensaje>")
 	public void mostrarMensajeErrorUsu() {
 		System.out.println("Error el usuario es incorrecto");
 	}
-	
+
 	@Then("contraseña es incorrecta, mostrar el mensaje <mensaje>")
 	public void mostrarMensajeErrorPwd() {
-	
+
 		System.out.println("Error la contraseña es incorrecto");
 	}
-	
 
-	
 }
