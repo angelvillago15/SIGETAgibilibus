b6d423df32e1acebacde033a6b85de9110d4d1ee
CristinaMarugan <cristina.marugan@alu.uclm.es>
2020-11-13 23:33:54 +0100

    Crear visualizarCalendario.java y borrar dependencias cucumber

---
+++ src/test/java/com/agibilibus/SIGET/visualizarCalendario.java
@@ -0,0 +1,19 @@
+package com.agibilibus.SIGET;
+
+import org.junit.Test;
+
+
+public class visualizarCalendario {
+	
+	@Test
+	public void testGetReuniones() {
+		
+	}
+	
+	@Test
+	public void testUsuarioSinReuniones() {
+
+	}
+	
+}
+	
