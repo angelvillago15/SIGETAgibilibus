d1b6f6430fd4e882e06e12161c6e18a1a84b9705
CristinaMarugan <cristina.marugan@alu.uclm.es>
2020-11-14 16:19:10 +0100

    Etiquetas que faltaban en el test

--- src/test/java/com/agibilibus/SIGET/TestVisualizarCalendario.java
+++ src/test/java/com/agibilibus/SIGET/TestVisualizarCalendario.java
@@ -4,23 +4,26 @@ import static org.junit.Assert.*;
 
 import org.joda.time.DateTime;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.agibilibus.SIGET.model.Usuario;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TestVisualizarCalendario {
-	/*
+	
 	@Test
 	public void testGetReuniones() {
-		Usuario cristina = new Usuario("Cristina", "99999999A", "Cristina", "Marugan", 666666666, "cristina@gmail.com", "00000000X", DateTime.parse("2020-11-30"), "usuario");
+		
 		try {
-			cristina.crearUsuario("99999999A", "Cristina", "cris", "pruebas", DateTime.parse("2020-11-30"), "99999999A", 666666666, "cristina@gmail.com");
+			Usuario cristina = Usuario.get().crearUsuario("99999999A", "Cristina", "cris", "pruebas", DateTime.parse("2020-11-30"), "99999999A", 666666666, "cristina@gmail.com");
 		} catch (Exception e) {
 			fail("Se ha lanzado una excepcion inesperada: " + e);
 		}
 		
-		
-	}*/
+	}
 	
 	@Test
 	public void testUsuarioSinReuniones() {
@@ -31,7 +34,7 @@ public class TestVisualizarCalendario {
 			//fail("Esperaba NoHayReunionesException");
 		} /*catch(NoHayReunionesException e) {
 			
-		}*/ catch (Exception e) {
+		} */catch (Exception e) {
 			fail("Se ha lanzado una excepcion inesperada: " + e);
 		}
 	}
