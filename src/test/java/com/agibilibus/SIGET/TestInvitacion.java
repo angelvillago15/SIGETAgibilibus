a08304b00b28fbddd3c81dec4bffe13d9d475bc8
Elisa <elisa.jimenez1@alu.uclm.es>
2020-11-15 15:54:08 +0100

    Testde las invitaciones terminado

--- src/test/java/com/agibilibus/SIGET/TestInvitacion.java
+++ src/test/java/com/agibilibus/SIGET/TestInvitacion.java
@@ -2,6 +2,7 @@ package com.agibilibus.SIGET;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -14,109 +15,102 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 import com.agibilibus.SIGET.controller.Controller;
-import com.agibilibus.SIGET.model.Invitacion;
-import com.agibilibus.SIGET.model.Reunion;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 
-class InvitacionTest {
+public class TestInvitacion {
 
 	@Autowired
 	private HttpSession sesion;
 
-	private Reunion r = new Reunion();
-	private Invitacion inv = new Invitacion();
-	
-	Controller controller = new Controller();
-	JSONObject credenciales1 = new JSONObject();
-	JSONObject datosReunion = new JSONObject();
-	JSONObject credenciales2 = new JSONObject();
+	private Controller controller = new Controller();
+	private Map<String, Object> datosReunion = new HashMap<String, Object>();
+	private Map<String, Object> credenciales1 = new HashMap<String, Object>();
+	private Map<String, Object> credenciales2 = new HashMap<String, Object>();
+	private String nombreTest = "test";
 
 	@Test
-	void TestCrearInvitacion() {
+	public void TestCrearYEnviarInvitacion() {
+		
+
+		Map<String, Object> credenciales1 = new HashMap<String, Object>();
+		credenciales1.put("userName", "Elisa");
+		credenciales1.put("pwd", "Seguridad2020");
 
 		try {
-			credenciales1.put("userName",  "Elisa");
-			credenciales1.put("pwd", "Seguridad2020");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
+			controller.login(sesion, credenciales1);
+		} catch (Exception e1) {
 			fail();
+
 		}
 
-		try {
-			controller.login(sesion, (Map<String, Object>) credenciales1);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			fail();
-		}
-		
-		try {
-			datosReunion.put("nombre", "graduacion");
-			datosReunion.put("fecha", "23/01/2021");
-			datosReunion.put("horaInicio", "10:00:00");
-			datosReunion.put("horaFin", "12:00:00");
-			datosReunion.put("descripcion", "graduacion");
-			datosReunion.put("url", "");
-			datosReunion.put("correos", "jaime@jaime.com");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			fail();
-		}
+		datosReunion.put("nombre", nombreTest);
+		datosReunion.put("fecha", "2020-12-01");
+		datosReunion.put("horaInicio", "10:00:00");
+		datosReunion.put("horaFin", "12:00:00");
+		datosReunion.put("descripcion", "test");
+		datosReunion.put("url", "https://www.youtube.com/?hl=es&gl=ES");
+		datosReunion.put("correos", "jaime@jaime.com");
 
-		try {
-			controller.guardarReunion(sesion, (Map<String, Object>) datosReunion);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			fail();
-		}
-		
-		
+
+				try {
+					controller.guardarReunion(sesion, datosReunion);
+				} catch (Exception e) {
+					fail();
+				}
+
+
+
+
 	}
-	
+
 	@Test
-	void TestRecibirInvitacion() {
-	
-	
-	try {
-		credenciales2.put("userName",  "Jaime");
+	public void TestRecibirInvitacion() {
+
+		credenciales2.put("userName", "jaime");
 		credenciales2.put("pwd", "Seguridad2020");
-	} catch (JSONException e) {
-		// TODO Auto-generated catch block
-		fail();
-	}
 
-	try {
-		controller.login(sesion, (Map<String, Object>) credenciales2);
-		
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		fail();
-	}
-	
-	String str = controller.getInvitaciones(sesion);
-	JSONObject jso;
-	try {
-		jso = new JSONObject(str);
-		JSONArray jsa = (JSONArray) jso.get("invitaciones");
-		
-		for(int i=0;i<jsa.length();i++) {
-			JSONObject o = (JSONObject) jsa.get(i);
+
+			try {
+				controller.login(sesion, (Map<String, Object>) credenciales2);
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				fail();
+			}
+
+
+
+		String invitaciones = controller.getInvitaciones(sesion);
+		JSONObject jso = null;
+
+		try {
+			jso = new JSONObject(invitaciones);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			fail();
 		}
-		
-		
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		fail();
-	}
-	}
-	
-	
-	
+		try {
+			JSONArray jsa = (JSONArray) jso.get("invitaciones");
+			boolean flag = false;
+			for (int i = 0; i < jsa.length(); i++) {
+				JSONObject invitacion = (JSONObject) jsa.get(i);
+				JSONObject reunion = (JSONObject) invitacion.get("reunion");
 
-	
+				if (reunion.get("title").equals(nombreTest)) {
+					flag = true;
+				}
+
+			}
+
+			assertTrue(flag);
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			fail();
+		}
+
+	}
+
 }
-
-
