c595cb5a96983a4660c61ba2108b96f7e39025cc
CarrivasUclm <carlos.rivas1@alu.uclm.es>
2020-11-16 22:43:49 +0100

    Mete usuarios desde el js

--- src/main/webapp/js/GestionUsuario.js
+++ src/main/webapp/js/GestionUsuario.js
@@ -1,17 +1,37 @@
 var buscador = $("#table").DataTable();
 
-$(document).ready(function() {
-    $("#input-search").keyup(function(){
-        buscador.search($(this).val()).draw();
 
-        if($("input-search").val()==""){
-            $(".content-search").fadeout();
-        }else{
-            $(".content-search").fadeIn();
-        }
-    }).focus(function() {
-        $("#table").show();
-    }).blur(function() {
-        $("#table").hide();
-    });
+$(document).ready(function() {
+	
+	$("#input-search").keyup(function() {
+		buscador.search($(this).val()).draw();
+
+		if ($("input-search").val() == "") {
+			$(".content-search").fadeout();
+		} else {
+			$(".content-search").fadeIn();
+		}
+	}).focus(function() {
+		$("#table").show();
+	}).blur(function() {
+		$("#table").hide();
+	});
+	cargarUsuarios();
+	
 });
+
+function cargarUsuarios() {
+	var tbodyRef = document.getElementById('tabla').getElementsByTagName('tbody')[0];
+	var newRow = tbodyRef.insertRow(0);
+
+	var newCell = newRow.insertCell(0);
+
+	newCell.innerHTML = "<tr><td><a href='javascript:datosUsuario();'>" + "hola" + "</a></td></tr>";
+
+
+}
+
+function datosUsuario() {
+	console.log("HOLA");
+
+}
