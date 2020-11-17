74994f55025ba4f60b9687686b4511ef009bed7e
PILAR <Pilar.Hernan@alu.uclm.es>
2020-11-17 20:58:26 +0100

    Vista finiquitada

--- src/main/webapp/js/invite.js
+++ src/main/webapp/js/invite.js
@@ -31,28 +31,52 @@ function validarCampo(campo) { // comprobar que no hay campos vacios
 
 var data2;
 function modificar() {
-	
-		var msg = {
-			type : "modificar Reunion",
-			id : $("#idReunion").val(),
-			nombre : $("#nombreReunion").val(),
-			fecha : $("#fecha").val(),
-			horaInicio :$("#horaInicio").val(),
-			horaFin : $("#horaFin").val(),
-			descripcion : $("#descripcion").val(),
-			url : $("#url").val(),
-			correos : $("#correos").val()
-		};
-		data2 = {
-			data2 : JSON.stringify(msg),
-			url : "modificarReunion",
-			type : "post",
-			contentType : 'application/json',
-			dataType : 'json'
-		}
-	
+
+	var msg = {
+		type : "modificar Reunion",
+		id : $("#idReunion").val(),
+		nombre : $("#nombreReunion").val(),
+		fecha : $("#fecha").val(),
+		horaInicio : $("#horaInicio").val(),
+		horaFin : $("#horaFin").val(),
+		descripcion : $("#descripcion").val(),
+		url : $("#url").val(),
+		correos : $("#correos").val()
+	};
+	data2 = {
+		data2 : JSON.stringify(msg),
+		url : "modificarReunion",
+		type : "post",
+		contentType : 'application/json',
+		dataType : 'json'
+	}
+
 	$.ajax(data2);
 	document.getElementById("correos").value = "";
 	alert('Se ha modificado la reunión');
 };
 
+var data3;
+function asistir() {
+	var opcion = confirm("¿Estás seguro que no desea asisitir a esta reunión?");
+	
+	if (opcion == true) {
+		var msg = {
+			type : "load Reunion",
+			id : $("#idReunion").val()
+		};
+		data3 = {
+			data3 : JSON.stringify(msg),
+			url : "eliminarReunionUsuario",
+			type : "post",
+			contentType : 'application/json',
+			dataType : 'json'
+		}
+
+		$.ajax(data3);
+		alert('Usted dejó de asistir a la reunión');
+	} else {
+		window.location.href = "UserCalendar.html";
+	}
+
+};
