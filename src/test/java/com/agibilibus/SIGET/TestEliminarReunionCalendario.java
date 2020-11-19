445f97c1958fd3d63b07d55cacc6f3981b655d70
Elisa <elisa.jimenez1@alu.uclm.es>
2020-11-19 12:55:06 +0100

    Cambios

--- src/test/java/com/agibilibus/SIGET/TestEliminarReunionCalendario.java
+++ src/test/java/com/agibilibus/SIGET/TestEliminarReunionCalendario.java
@@ -1,56 +1,87 @@
 package com.agibilibus.SIGET;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.agibilibus.SIGET.controller.Controller;
+import com.agibilibus.SIGET.dao.ReunionDAO;
+import com.agibilibus.SIGET.dao.UserDAO;
+import com.agibilibus.SIGET.model.Reunion;
+import com.agibilibus.SIGET.model.Usuario;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.jupiter.api.Test;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.json.JSONObject;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.agibilibus.SIGET.controller.Controller;
-import com.agibilibus.SIGET.dao.ReunionDAO;
-import com.agibilibus.SIGET.dao.UserDAO;
-import com.agibilibus.SIGET.model.Reunion;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestEliminarReunionCalendario {
 
+public class TestEliminarReunionCalendario {
 	private Controller controller = new Controller();
 
 	@Autowired
 	private UserDAO userdao;
+	
 	@Autowired
 	private ReunionDAO reuniondao;
+	
+	private Reunion r;
 
-	@Test
-	public void testEliminarReunion() {
+	MockHttpSession session = new MockHttpSession();
+	Map<String, Object> reunion = new HashMap<String, Object>();
 
-		Map<String, Object> reunion = new HashMap<String, Object>();
+	@Before
+	public void init() throws Exception {
 		MockHttpSession session = new MockHttpSession();
-
-		session.setAttribute("user", userdao.findById("carlos").get());
-		reunion.put("id", "prueba");
-		reunion.put("nombre", "Mañanera");
+		Usuario u = userdao.findById("carlos").get();
+		session.setAttribute("user", u);
+		reunion.put("nombre", "EliminarReunion");
 		reunion.put("fecha", "2020-10-01");
 		reunion.put("horaInicio", "11:00:00");
 		reunion.put("horaFin", "13:00:00");
-		reunion.put("descripcion", "Fallos");
+		reunion.put("descripcion", "Hola");
 		reunion.put("url", "https://www.google.com/");
-		reunion.put("correos", "a@gmail.com");
-
-		try {
-			controller.guardarReunion(session, reunion);
-			reuniondao.deleteById(((Reunion) reunion).getIdReunion());
-		} catch (Exception e) {
-			fail("Error creando reunion: " + e);
-
-		}
+		
+		
 	}
+
+	@Test
+	public void testOrganizador() throws Exception {
+		reunion.put("correos", "pepa@pepa.com");
+		System.out.println(reunion);
+		controller.guardarReunion(session, reunion);
+		JSONObject jso = new JSONObject(controller.getReuniones(session));
+		System.out.println(jso.get("id"));
+		//aceptan invitacion
+		//controller.eliminarReunionUsuario(session, reunion);
+		
+		
+	}
+
+	/*@Test
+	public void testAsistente() {
+		//Asistente elimina reunión
+		session.setAttribute("user", userdao.findById("Pepa"));
+		reunion.put("correos", "pepa@pepa.com, elisa@elisa.com");
+		//aceptan invitacion
+		controller.eliminarReunionUsuario(session, reunion);
+
+	}
+
+	@Test
+	public void testUltimoOrganizador() {
+		//Último organizador elimina reunión, la reunión se borra de la bbdd
+		reunion.put("correos", "");
+		controller.eliminarReunionUsuario(session, reunion);
+
+	}*/
 
 }
