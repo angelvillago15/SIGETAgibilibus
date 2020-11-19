de02edd344a53546c1ac605aa76fd39a9b4e5d67
Jaimej5 <43786797+Jaimej5@users.noreply.github.com>
2020-11-19 22:54:55 +0100

    Revert "Revert "empiezo""
    This reverts commit d6e52dafc6f8ab8e6d04c0169fc0a69c8f67b21f.

--- src/main/webapp/js/GestionUsuario.js
+++ src/main/webapp/js/GestionUsuario.js
@@ -26,9 +26,16 @@ function getUsuarios() {
 
 		success: function(response) {
 			var datos = JSON.parse(response);
+<<<<<<< Updated upstream
 
 			for (var i in datos.usuarios) {
 				var txt = "<tr><td><a href='javascript:mostrarUsuario(\"" + datos.usuarios[i].user + "\");'>" + datos.usuarios[i].user + "</a></td></tr>";
+=======
+			var txt = "";
+			
+			for (var i in datos.usuarios) {
+				txt = txt + "<tr><td><a href='javascript:mostrarUsuario(\""+ datos.usuarios[i].user +"\");'>" + datos.usuarios[i].user + "</a></td></tr>";
+>>>>>>> Stashed changes
 				buscador.row.add([txt]);
 			}
 		},
@@ -60,8 +67,23 @@ function mostrarUsuario(username){
 				$('#userTelf').value(response.telefono);
 				$('#userDate').value(response.nacimiento);
 
+<<<<<<< Updated upstream
 			},
 			error : function(response) {
+=======
+			success: function(response) {
+				document.getElementById("userDni").value=usuario.userDni;
+				document.getElementById("userCompletName").value=usuario.userCompletName;
+				document.getElementById("userApellidos").value=usuario.userApellidos;
+				document.getElementById("userTelf").value=usuario.userTelf;
+				document.getElementById("userDate").value=usuario.userDate;
+				document.getElementById("userName").value=usuario.user;
+				document.getElementById("pwd1").value=usuario.pwd1;
+				document.getElementById("userMail").value=usuario.userMail;	
+	
+			},
+			error: function(response) {
+>>>>>>> Stashed changes
 				alert(response.message);
 			}
 		}
