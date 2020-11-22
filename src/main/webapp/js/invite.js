ef8fc9176bbe5c66eb94251cce069bee31f8f520
ManuelPuente <43786852+ManuelPuente@users.noreply.github.com>
2020-11-22 23:50:19 +0100

    Control de los campos en los formularios y arreglos varios

--- src/main/webapp/js/invite.js
+++ src/main/webapp/js/invite.js
@@ -28,9 +28,25 @@ function validarCampo(campo) { // comprobar que no hay campos vacios
 	}
 	return valido;
 };
+function fechaCorrecta() {
+    var correcto = new Boolean(true); 
+    var fecha=$("#fecha").val();
+    var fechaActual = new Date();
+    var hora=$("#horaInicio").val();
+    var fechaFormulario= new Date(fecha.substring(0,4),fecha.substring(5,7)-1,fecha.substring(8,10),hora.substring(0,2),hora.substring(3,5));
+    if(false==(fechaFormulario >= fechaActual )){ // la fecha del formulario es menor a la actual (en este momento)
+        alert('La fecha seleccionada es anterior a la actual');
+        correcto=false;
+    }
 
+    if($("#horaInicio").val() > $("#horaFin").val() ){ //la hora de inicio es mayor que la de fin
+        correcto=false;
+        alert("La hora de fin no puede ser anterior a la de inicio")
+    }
+    return correcto;
+  }
 function modificar() {
-	if (validarCampo("fecha") && validarCampo("horaFin") && validarCampo("horaInicio") && validarCampo("nombreReunion")) {
+	if (validarCampo("fecha") && validarCampo("horaFin") && validarCampo("horaInicio") && validarCampo("nombreReunion") && fechaCorrecta()) {
 
 	var msg = {
 		type : "modificar Reunion",
