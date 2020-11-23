25909d6b969135f9edfc0ff558574a120297d335
Elisa <elisa.jimenez1@alu.uclm.es>
2020-11-23 17:45:00 +0100

    Test vista del registro

--- src/test/java/com/agibilibus/siget/pruebasSelenium.java
+++ src/test/java/com/agibilibus/siget/pruebasSelenium.java
@@ -2,62 +2,105 @@ package com.agibilibus.siget;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import junit.framework.Assert;
+
+
 
 public class pruebasSelenium {
 
- private WebDriver driver;
+	private WebDriver driver;
 
- @Before
- public void setUp() {
+	@Before
+	public void setUp() {
 
-  System.setProperty("webdriver.chrome.driver", "./src/test/resources/ChromeDriver/chromedriver.exe");
-  driver = new ChromeDriver();
-  driver.manage().window().maximize();
-  driver.get("https://sigetagibilibus.herokuapp.com/Login.html");
- 
+		System.setProperty("webdriver.chrome.driver", "./src/test/resources/ChromeDriver/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://sigetagibilibus.herokuapp.com/Login.html");
 
- }
+	}
+
+	@Test
+	public void testLoginPage() {
+
+		WebElement nombreUsuario = driver.findElement(By.id("username"));
+		WebElement contraseña = driver.findElement(By.id("pass"));
+		WebElement btnLogin = driver.findElement(By.id("login"));
+
+		nombreUsuario.clear();
+		nombreUsuario.sendKeys("Pilar");
+
+		contraseña.clear();
+		contraseña.sendKeys("Hola1234");
+
+		btnLogin.click();
+
+		String actualUrl = "https://sigetagibilibus.herokuapp.com/Login.html";
+		String expectedUrl = driver.getCurrentUrl();
+
+		assertEquals(actualUrl, expectedUrl);
+
+	}
+
+	@Test
+	public void testRegisterPage() {
+		WebDriverWait wait = new WebDriverWait(driver,1);;
+		driver.get("https://sigetagibilibus.herokuapp.com/Register.html");
+
+		WebElement dni = driver.findElement(By.id("userDni"));
+		dni.sendKeys("70707070A");
+		
+		WebElement nombreCompleto = driver.findElement(By.id("userCompletName"));
+		nombreCompleto.sendKeys("SeleniumRegister");
+		
+		WebElement apellidos = driver.findElement(By.id("userApellidos"));
+		apellidos.sendKeys("Garcia Garcia");
+		
+		WebElement telefono = driver.findElement(By.id("userTelf"));
+		telefono.sendKeys("655655655");
+		
+		WebElement fecha = driver.findElement(By.id("userDate"));
+		fecha.sendKeys("06-12-1990");
+		
+		WebElement nombre  = driver.findElement(By.id("userName"));
+		nombre.sendKeys("SeleniumRegister");
+		
+		WebElement pass = driver.findElement(By.id("pwd1"));
+		pass.sendKeys("Seguridad2020");
+		
+		WebElement validarPass = driver.findElement(By.id("pwd2"));
+		validarPass.sendKeys("Seguridad2020");
+		
+		WebElement mail = driver.findElement(By.id("userMail"));
+		mail.sendKeys("register@register.com");
+		
+		WebElement validarMail = driver.findElement(By.id("mail2"));
+		validarMail.sendKeys("register@register.com");
+		
+		WebElement boton = driver.findElement(By.id("register"));
+		boton.click();
+		
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		alert = driver.switchTo().alert();
+		String textoAlerta = alert.getText();
+		String textoEsperado = "Usuario registrado correctamente";
 
 
- @Test
- public void testLoginPage() {
+		assertEquals(textoAlerta, textoEsperado);
+	}
 
-  WebElement nombreUsuario = driver.findElement(By.id("username"));
-  WebElement contraseña = driver.findElement(By.id("pass"));
-  WebElement btnLogin = driver.findElement(By.id("login"));
-
-  nombreUsuario.clear();
-  nombreUsuario.sendKeys("Pilar");
-
-  
-  contraseña.clear();
-  contraseña.sendKeys("Hola1234");
-
-  
-  btnLogin.click();
-  
-String actualUrl ="https://sigetagibilibus.herokuapp.com/Login.html";
-String expectedUrl = driver.getCurrentUrl();
-
-assertEquals(actualUrl,expectedUrl);
-  
-
- }
-
- @After
- public void tearDown() {
-  driver.quit();
- }
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
 }
