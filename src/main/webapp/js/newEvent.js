07b385ca131f8f5e674b5a99e0c6ed1cc4333f52
ManuelPuente <43786852+ManuelPuente@users.noreply.github.com>
2020-11-23 22:17:01 +0100

    arreglo de la rama

--- src/main/webapp/js/newEvent.js
+++ src/main/webapp/js/newEvent.js
@@ -1,7 +1,7 @@
 var data;
 function crear () { // se le llama cuando se da al boton crear
     
-    if(false!=(validarCampo("nombreReunion") && validarCampo("fecha") && validarCampo("horaInicio")&& validarCampo("horaFin") && validarCampo("descripcion") && validarCampo("url") && validarCampo("invitar"))){ // no hay un campo vacio
+    if((validarCampo("nombreReunion") && validarCampo("fecha") && validarCampo("horaInicio")&& validarCampo("horaFin") && validarCampo("descripcion") && validarCampo("url") && validarCampo("invitar"))){ // no hay un campo vacio
         if(fechaCorrecta()){ //se comprueba la fecha
             var reunion= new Reunion($("#nombreReunion").val(),$("#fecha").val(),$("#horaInicio").val(),$("#horaFin").val(),$("#descripcion").val(),$("#url").val(),$("#invitar").val()); // crear objeto reunion            
             reunion.crearReunion();
